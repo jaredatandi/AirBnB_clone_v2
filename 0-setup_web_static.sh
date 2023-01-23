960d8bd2c63f4a96a8bf99d295125873214b1e2f
jaredatandi <jaredatandi07@gmail.com>
2023-01-23 22:03:40 +0300

    Use a custom domain name

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 # setup for deployment
-sudo apt-get -y update
-sudo apt-get -y install nginx
+if ! [ -x "$(command -v nginx)" ]; then
+	sudo apt-get -y update
+	sudo apt-get -y install nginx
+fi
+
 SRC="/etc/nginx/sites-available/default"
 STATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static_current/;\n\t}\n"
 sudo mkdir -p "/data/web_static/releases/test/"
 sudo mkdir "/data/web_static/shared/"
 echo "Hello World" | sudo tee "/data/web_static/releases/test/index.html"
+
+if [ -L /data/web_static/current ]; then
+  rm -f /data/web_static/current
+fi
+
 sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
 sudo chown -R ubuntu:ubuntu "/data/"
-sudo sed -i "35i $STATIC" $SRC
+
+echo "server {
+  listen 80;
+  server_name jaredatandi.tech;
+
+  location /hbnb_static {
+    alias /data/web_static/current/;
+  }
+}" > /etc/nginx/sites-available/hbnb_static
+ln -s /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/
+
 sudo service nginx restart
