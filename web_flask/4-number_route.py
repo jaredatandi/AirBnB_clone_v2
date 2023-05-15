7746a681244023c07a1f98dac51aa96058aa12f9
jaredatandi <jaredatandi07@gmail.com>
2023-05-15 19:45:48 +0300

    Perform checks

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python3
 """Flask"""
+
+
 from flask import Flask
 app = Flask(__name__)
+app.url_map.merge_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_route():
-    """hello_route"""
+    """Hello"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """hbnb"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """c_route"""
-    return "C {}".format(text.replace("_", " "))
+@app.route('/c/<text>')
+def text(text):
+    """C route"""
+    text = text.replace('_', ' ')
+    return "C %s" % text
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    """python_route"""
-    return "Python {}".format(text.replace("_", " "))
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python(text):
+    """Use defaults"""
+    formatted_text = text.replace('_', " ")
+    return "Python %s" % formatted_text
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """number_route"""
+@app.route('/number/<int:n>')
+def check_number(n):
     if isinstance(n, int):
-        return "{} is a number".format(n)
+        return "%d is a number" % n
 
 
 if __name__ == "__main__":
