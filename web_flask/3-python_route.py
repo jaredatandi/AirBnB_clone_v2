c6514068911cd1bb43f244da69f4c73eccbc7de2
jaredatandi <jaredatandi07@gmail.com>
2023-05-15 19:30:02 +0300

    Add a newline

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,31 +2,26 @@
 """Flask"""
 from flask import Flask
 app = Flask(__name__)
+app.url_map.merge_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_route():
-    """hello_route"""
+    """Hello"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """hbnb"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """c_route"""
-    return "C {}".format(text.replace("_", " "))
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    """python_route"""
-    return "Python {}".format(text.replace("_", " "))
+@app.route('/c/<text>')
+def text(text):
+    """C route"""
+    text = text.replace('_', ' ')
+    return "C %s" % text
 
 
 if __name__ == "__main__":
