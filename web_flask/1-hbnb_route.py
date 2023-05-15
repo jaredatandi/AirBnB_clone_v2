38253e752a4d32603aed0158d47b5b9a480d5a1f
jaredatandi <jaredatandi07@gmail.com>
2023-05-15 19:16:50 +0300

    Add variable

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
+"""Flask
+
+    Returns:
+        text: loads a basic text to html
+            through the route /c/<text> 
+"""
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_HBNB():
+    """hello
+
+    Returns:
+        text: hello hbnb
+    """
     return 'Hello HBNB'
 
 @app.route('/hbnb')
 def hbnb():
+    """hbnb"""
     return 'HBNB'
 
 if __name__ == "__main__":
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+"""Flask"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+app.url_map.merge_slashes = False
 
 
 @app.route('/')
-def hello():
-    return 'Hello HBNB'
+def hello_route():
+    """Hello"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb')
 def hbnb():
-    return 'HBNB'
+    """hbnb"""
+    return "HBNB"
 
 
 @app.route('/c/<text>')
 def text(text):
-    text.replace('_', ' ')
-    return  'C %s' % escape(text)
+    """C route"""
+    text = text.replace('_', ' ')
+    return "C %s" % text 
 
-
-if __name__ == '__name__':
+if __name__ == "__main__":
     app.run()
+
+
