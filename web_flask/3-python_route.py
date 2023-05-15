0bd3bb35e50b2aebfc378382e62b094b08b9be7d
jaredatandi <jaredatandi07@gmail.com>
2023-05-15 19:39:27 +0300

    Use default text

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Flask"""
+
+
 from flask import Flask
 app = Flask(__name__)
 app.url_map.merge_slashes = False
@@ -22,6 +24,13 @@ def text(text):
     """C route"""
     text = text.replace('_', ' ')
     return "C %s" % text
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python(text):
+    formatted_text = text.replace('_', " ")
+    return "Python %s" % formatted_text
 
 
 if __name__ == "__main__":
