fefc3b560b0eaf5003c1c0207878720c0bf47e6c
jaredatandi <jaredatandi07@gmail.com>
2023-05-19 04:41:49 +0300

    Update the file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" Flask"""
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_world():
+    """Hello route"""
     return 'Hello HBNB'
